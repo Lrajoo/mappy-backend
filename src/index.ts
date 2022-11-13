237b9fcd5dfb9efdd66cdb87de0030e90b3fc0eb
Lingess Rajoo <Lrajoo@purdue.edu>
2022-11-13 00:40:26 -0800

    Updated cors to frontend url

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://main.dgt48bo9ztida.amplifyapp.com/",
+    // origin: "http://localhost:3000",
   })
 );
 app.use(express.json());
